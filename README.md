16536b1138d3fcac9c152354de84ce78c2030e41
Rifki Idnan Haikal Fajar <rifkiihf1998@gmail.com>
2025-02-08 13:54:05 +0700

    final

--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# USATUHIR
-=======
-# Simple- Aplikasi Bimbingan Skripsi Online PHP Native
->>>>>>> 21b8353 (Initial commit)
--- config/databases.php
+++ config/databases.php
@@ -3,6 +3,6 @@ date_default_timezone_set('Asia/Jakarta');
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "usatuhir";
+$dbname = "db_usatuhir";
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
